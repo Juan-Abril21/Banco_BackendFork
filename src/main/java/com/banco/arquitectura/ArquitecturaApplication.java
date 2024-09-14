aa21f80e43f529bfa8a90fdcc0b3a72b74fd294c
BryamDigar <bryamdiaz1010@gmail.com>
2024-09-13 19:20:00 -0500

    First commit

---
+++ src/main/java/com/banco/arquitectura/ArquitecturaApplication.java
@@ -0,0 +1,13 @@
+package com.banco.arquitectura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaApplication.class, args);
+	}
+
+}
